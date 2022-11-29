c98e63acd6178d8426b95c36a2abfe5fbb1048b0
Davis Smith <44070645+davissmith1@users.noreply.github.com>
2022-11-29 15:07:39 -0500

    import fixes pt.3

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import { roll } from "./lib/roll.js"
 import minimist from "minimist";
 import express from "express";
 
-const args = require('minimist')(process.argv.slice(2));
+const args = process.argv.slice(2);
 const port = args.port || 5000
 
 const app = express();
